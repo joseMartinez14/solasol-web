9f16d68092d4bc35b47e2318b02e113e2fb93718
Jose Martinez <josemartinez@Joses-MacBook-Pro.local>
2024-05-26 11:25:22 -0600

    jordan grid suggestion

--- src/components/catalog/showControls/index.tsx
+++ src/components/catalog/showControls/index.tsx
@@ -24,57 +24,65 @@ const ShowControls = (props: ShowControlsProps) => {
 
     return (
         <Box pt={2} px={3} width={"100%"}>
-            <Box sx={{ width: '100%', height: '100%' }} display={'flex'} flexDirection={'row'}>
-                <Box sx={{ width: '60%', height: '100%' }} >
-                    <Typography
-                        variant="subtitle2"
-                        gutterBottom
-                        sx={{
-                            color: COLORS.black,
-                            fontSize: '22px',
-                            margin: 0,
-                            pb: 1,
-                            fontWeight: 600,
-                        }}>
-                        {"Filtrar por categorias:"}
-                    </Typography>
-                    <Grid container spacing={1.5} sx={{ width: "100%" }}>
-                        {filters.map((item, index) => (
-                            <Grid item lg={3} md={6} xs={6} key={index}>
-                                <Paper elevation={1} sx={{ p: 0.5, backgroundColor: selectedFiltersData.includes(item.name) ? '#CBFCE5' : '' }} >
-                                    <Box height={'100%'} width={'100%'}>
-                                        <div onClick={() => onClick(item.id, item.name)}>
-                                            <Typography
-                                                variant="subtitle2"
-                                                gutterBottom
-                                                textAlign={'center'}
-                                                sx={{
-                                                    color: COLORS.black,
-                                                    fontSize: '18px',
-                                                    margin: 0,
-                                                    padding: 0,
-                                                    fontWeight: 500,
-                                                }}>
-                                                {item.name}
-                                            </Typography>
-                                        </div>
-                                    </Box>
-                                </Paper>
-                            </Grid>
-                        ))}
-                    </Grid>
-                </Box>
-                <Box sx={{ width: '40%', height: '100%' }} >
-                    <FormControl fullWidth>
-                        <InputLabel id="select-label-1">Order by</InputLabel>
-                        <Select labelId="select-label-1" label="Select 1" onChange={(item) => { onOrderByChange(item.target.value as string); }} >
-                            <MenuItem value="price-desc">Precio: menor a mayor</MenuItem>
-                            <MenuItem value="price-asc">Precio: mayor a menor</MenuItem>
-                            {/* <MenuItem value="date-new">Más reciente</MenuItem> */}
-                        </Select>
-                    </FormControl>
-                </Box>
-            </Box>
+            <Grid container spacing={1.5} sx={{ width: "100%" }}>
+                <Grid item md={6} xs={12}>
+                    <Box >
+                        <FormControl fullWidth>
+                            <InputLabel id="select-label-1">Order by</InputLabel>
+                            <Select labelId="select-label-1" label="Select 1" onChange={(item) => { onOrderByChange(item.target.value as string); }} >
+                                <MenuItem value="price-desc">Precio: menor a mayor</MenuItem>
+                                <MenuItem value="price-asc">Precio: mayor a menor</MenuItem>
+                                {/* <MenuItem value="date-new">Más reciente</MenuItem> */}
+                            </Select>
+                        </FormControl>
+                    </Box>
+                </Grid>
+                <Grid item md={6} xs={12}>
+                    <Box>
+                        <Typography
+                            variant="subtitle2"
+                            gutterBottom
+                            sx={{
+                                color: COLORS.black,
+                                fontSize: '22px',
+                                margin: 0,
+                                pb: 1,
+                                fontWeight: 600,
+                            }}>
+                            {"Filtrar por categorias:"}
+                        </Typography>
+                        <Grid container spacing={1.5} sx={{ width: "100%" }}>
+                            {filters.map((item, index) => (
+                                <Grid item lg={3} md={6} xs={6} key={index}>
+                                    <Paper elevation={1} sx={{ p: 0.5, backgroundColor: selectedFiltersData.includes(item.name) ? '#CBFCE5' : '' }} >
+                                        <Box height={'100%'} width={'100%'}>
+                                            <div onClick={() => onClick(item.id, item.name)}>
+                                                <Typography
+                                                    variant="subtitle2"
+                                                    gutterBottom
+                                                    textAlign={'center'}
+                                                    sx={{
+                                                        color: COLORS.black,
+                                                        fontSize: '18px',
+                                                        margin: 0,
+                                                        padding: 0,
+                                                        fontWeight: 500,
+                                                    }}>
+                                                    {item.name}
+                                                </Typography>
+                                            </div>
+                                        </Box>
+                                    </Paper>
+                                </Grid>
+                            ))}
+                        </Grid>
+                    </Box>
+                </Grid>
+
+            </Grid>
+
+
+
         </Box>
     )
 }
